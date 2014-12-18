3733d883a57a92a825fc1f69f5ee2bd3c3282beb
emmmbrown <ebrown118@gmail.com>
2014-12-17 21:52:49 -0800

    My solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This creates a pair of functions which cache the inverse
+# of a matrix in order to conserve energy spent in its computation.
+# It is assumed that the matrix is invertible
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Creates a 'matrix' object that can cache its inverse
+    inv <- NULL
+    # Define object methods
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    # Create list of methods/'matrix' object
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Returns a matrix that is the inverse of 'x'
+  # 'x' is an object returned by makeCacheMatrix
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+      message('Getting cached data')
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
